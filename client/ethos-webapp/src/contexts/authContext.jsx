78242e414eec86c645cc400a58e0cf7b454c8d20
Arianna Smith-Forte <ariannasforte@meta.com>
2025-07-13 18:31:34 -0700

    bug/render errors

--- client/ethos-webapp/src/contexts/authContext.jsx
+++ client/ethos-webapp/src/contexts/authContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { supabase } from '../services/supabaseClients';
+import { supabase } from '../services/supabaseClients.jsx';
 
 
 const AuthContext = createContext({});
